d9f92a43d8e862cade14725477bbab3ec5ef56cf
Ryan Brady <ryanbrady369@gmail.com>
2019-01-15 00:12:11 +0000

    Automatically backed up by Learn

--- lib/student.rb
+++ lib/student.rb
@@ -8,7 +8,7 @@ class Student
     @id = id
   end
 
-  def self.save
+  def save
     DB[:conn].execute("INSERT INTO students (name, grade) VALUES (?, ?)", self.name, self.grade)
     #@id = DB[:conn].execute("SELECT last_insert_rowid() FROM songs")[0][0]
   end
